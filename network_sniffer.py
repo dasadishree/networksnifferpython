9e679f86903effeab25534e0dfc3046c6ddabac3
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2024-08-09 23:59:29 -0400

    Add files via upload

--- network_sniffer.py
+++ network_sniffer.py
@@ -6,9 +6,50 @@ import textwrap
 def ethernet_frame(data):
     #unpack data (1st 14 bytes of ethernet frame into 2 6byte strings for MAC addresses and 1 2byte value)
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
-    
+    #convert binary to readble format
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
 
+#function to format mac addresses to readable format
 def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
+
+#unpack IPv4 layer
+def ipv4_packet(data):
+    version_header_length = data[0]
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+#format IP addresses 
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+#unpack ICMP packets
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+#unpack TCP packets
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    return src_port, dest_port, sequence, acknowledgment, data[offset:]
+
+#unpack UDP packets
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+#format readable output
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ' '.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+
+#create raw socket
+conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
